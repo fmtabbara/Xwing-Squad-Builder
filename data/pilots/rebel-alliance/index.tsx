1a803e24b86eec819967e98a291d7c53a60fffb8
fmtabbara <fmtabbara@hotmail.co.uk>
2020-10-11 00:04:17 +0100

    Updates

---
+++ data/pilots/rebel-alliance/index.tsx
@@ -0,0 +1,9 @@
+import arc from "./arc-170-starfighter.json";
+import bwing from "./a-sf-01-b-wing.json";
+import attackShuttle from "./attack-shuttle.json";
+import auzituk from "./auzituck-gunship.json";
+import yWing from "./btl-a4-y-wing.json";
+import kWing from "./btl-s8-k-wing.json";
+
+const ships = [arc, bwing, attackShuttle, auzituk, yWing, kWing];
+export { ships };
---
+++ precache-manifest.de73fe4fdf08aba5ceb9f9b8ecb316a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2651485b6896b4198e01d45779e426b",
+    "url": "/xwing-squad-builder/index.html"
+  },
+  {
+    "revision": "6bf7897c1408d4594fea",
+    "url": "/xwing-squad-builder/static/css/main.33a3a5af.chunk.css"
+  },
+  {
+    "revision": "b03b418f679029038cfa",
+    "url": "/xwing-squad-builder/static/js/2.6be10e47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/xwing-squad-builder/static/js/2.6be10e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf7897c1408d4594fea",
+    "url": "/xwing-squad-builder/static/js/main.48ccb468.chunk.js"
+  },
+  {
+    "revision": "41ac9969b74a682ef173",
+    "url": "/xwing-squad-builder/static/js/runtime-main.28fbc71f.js"
+  },
+  {
+    "revision": "19be28cc9a2542ae4bd7a0301ed08468",
+    "url": "/xwing-squad-builder/static/media/SWZ01_logo.19be28cc.png"
+  },
+  {
+    "revision": "7163256f49c13014de58c8a02daa21b1",
+    "url": "/xwing-squad-builder/static/media/xwing-miniatures-ships.7163256f.ttf"
+  },
+  {
+    "revision": "f9e5ad11ee75a9baa385f256c9ef595e",
+    "url": "/xwing-squad-builder/static/media/xwing-miniatures.f9e5ad11.ttf"
+  }
+]);
